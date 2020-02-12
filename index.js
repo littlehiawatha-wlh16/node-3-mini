4c65f2339edf4b84ed415b22b6a0a85ce2d4c132
Jacob Hall <halljaco@yahoo.com>
2020-02-12 13:55:32 -0700

    Node-3 mini 5.3.mp

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+// general rule of thumb is to bring in dependencies in order of their need for the application to run
+// require('dotenv').config({path: __dirname + '/../.env'});  ---> for hosting reference
+require('dotenv').config();
 const express = require("express");
+const massive = require('massive');
+const controller = require('./controller')
 
 const app = express();
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+console.log('SERVER_PORT:', SERVER_PORT, 'CONNECTION:', CONNECTION_STRING)
+
+massive(CONNECTION_STRING).then(dbObj => {
+  app.set('db', dbObj)
+  // console.log(dbInstance)
+
+  // app = {                          <-----app.set does this
+  //   db: dbObj
+  // }
+
+  // dbInstance.new_planes().then(
+  //   planes => console.log(planes[0])
+  // ).catch(err => console.log(err))
+
+})
 
 app.use(express.json());
+
+app.get('/api/planes', controller.getPlanes)
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT}`);
 });
+
